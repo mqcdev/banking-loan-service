985fa1e9c4325e79e8d6b7aa375595b51139af7c
mqcdev <michael.chavezjq@gmail.com>
2025-06-19 21:55:30 -0500

    feat(config): add configurations for the web client

---
+++ src/main/java/com/nttdata/banking/loan/config/WebClientConfig.java
@@ -0,0 +1,30 @@
+package com.nttdata.banking.loan.config;
+
+import com.nttdata.banking.loan.util.Constants;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Data
+@NoArgsConstructor
+public class WebClientConfig {
+    private static final Logger logger = LoggerFactory.getLogger(WebClientConfig.class);
+    private WebClient webclient;
+    private String url = Constants.TEXTO_VACIO;
+
+    public Mono<String> setUriData(String getUrl){
+
+        return Mono.just(getUrl).flatMap(
+                u ->{
+                    if(!url.equals(getUrl)){
+                        this.webclient =  WebClient.builder().baseUrl(u).build();
+                        setUrl(getUrl);
+                    }
+                    return Mono.just(u);
+                }
+        );
+    }
+}
